0072a98dcb70b1d1f1320f61355eae930206bdb1
Sara E. Hansen <71297221+hanse2s@users.noreply.github.com>
2024-08-01 09:27:47 -0400

    Update MicrosoftAccessWithR.R
    Cleaning up the code and adding more explanations

--- code/MicrosoftAccessWithR.R
+++ code/MicrosoftAccessWithR.R
@@ -1,7 +1,7 @@
 # Working with Microsoft Access data in R
 
 ## Sara Hansen, hanse2s
-## Modified April 12, 2024
+## Modified August 1, 2024
 
 library(tidyverse) # to write nice code
 library(odbc) # to check drivers
@@ -17,13 +17,13 @@ library(RODBC) # to connect to Access
 # using a small subset of the Prairie Fen Biodiversity Project Database
 # available via GBIF: https://www.gbif.org/dataset/72c4d3c6-5b8d-49f5-bfbe-febd53849588
 
-################################################################################
+# # # 
 
-# Step 1: Read in the flat file
+# Step 1: Read in the flat file ------------------------------------------------
 dat <- read.csv("data/prairie-fen-data-flat.csv", header = TRUE, 
                 fileEncoding = "UTF-8-BOM")
 
-# Step 2: Explore data
+# Step 2: Explore data ---------------------------------------------------------
 glimpse(dat)
 # We have three main identifiers: occurrenceID, eventID, and locationID
 # This tells us we have data that are arranged based on some physical hierarchy
@@ -32,7 +32,7 @@ glimpse(dat)
 
 # This is a good time for a relational database!
 
-# Step 3: Set up a relational database in R
+# Step 3: Set up a relational database in R ------------------------------------
 # We'll set up three distinct files that minimize redundancy
 location <- dat %>%
   select(locationID, locality, countryCode, stateProvince, county) %>%
@@ -74,25 +74,25 @@ occurrence <- occurrence %>% select(-occurrenceID)
 # to three smaller data frames that collectively take about 114,000 bytes
 # Imagine how much we could reduce storage space of massive data sets!
 
-################################################################################
+# # #
 
 # Now we are ready to export these data to an MS Access database for safekeeping
 
-# Step 4: Check Access drivers
+# Step 4: Check Access drivers -------------------------------------------------
 odbc::odbcListDrivers()
 # If the "Microsoft Access Driver" isn't listed, download and install it to your computer
 
-# Step 5: Create a blank Access database in Access
+# Step 5: Create a blank Access database in Access -----------------------------
 # Then connect to it here by providing the file path
 # A blank Access database is already available in the data folder for this project
 database <- "data/prairie-fen-database.accdb"
 connection <- odbcConnectAccess2007(database)
 
-# Step 6: Write the database out into the blank Access database
+# Step 6: Write the database out into the blank Access database ----------------
 # Below function checks whether a table already exists and deletes it if yes,
 # then saves the table to Access
-# If the string length is equal to or exceeds 255, the type is automatically set to "LONGTEXT",
-# otherwise it is "VARCHAR(255)" which is the default
+# The type and length of each column determines the data type used to write it out
+
 writeOut <- function(connection, x) {
   
   if(deparse(substitute(x)) %in% sqlTables(connection)$TABLE_NAME) {
@@ -103,7 +103,14 @@ writeOut <- function(connection, x) {
     as.data.frame() %>%
     pivot_longer(cols = everything(),
                  names_to = "column", values_to = "max_length") %>%
-    mutate(columnType = case_when(max_length < 255 ~ "VARCHAR(255)",
+    left_join(purrr::map(x, class) %>% 
+                as.data.frame() %>%
+                pivot_longer(cols = everything(),
+                             names_to = "column", values_to = "class"),
+              by = "column") %>%
+    mutate(columnType = case_when(max_length < 255 & class == "character" ~ "VARCHAR(255)",
+                                  max_length < 255 & class == "integer" ~ "INT",
+                                  max_length < 255 & class == "numeric" ~ "DOUBLE",
                                   max_length >= 255 ~ "LONGTEXT")) %>%
     pull(columnType)
   
@@ -114,33 +121,28 @@ writeOut <- function(connection, x) {
   # note that Access drivers don't support primary key creation
 }
 
-# Alternatively, you could specify column types individually
-# But keeping columns as character format is often preferable to avoid automatic edits
-
-#sqlSave(connection, event, varTypes = 
-          #c(eventID = "VARCHAR(255)", eventDate = "DATE",
-            #year = "INT", month = "INT", day = "INT",
-            #decimalLatitude = "FLOAT", decimalLongitude = "FLOAT",
-            #locationID = "VARCHAR(255)"))
-
 # Write out each table to the Access database
 writeOut(connection, location)
 writeOut(connection, event)
 writeOut(connection, occurrence)
 
-################################################################################
+# Close the connection to the database
+odbcClose(connection)
+rm(database)
 
-# You now have an Access database! Check it out in Access
+# # #
 
-################################################################################
+# You now have an Access database! Check it out in Access ----------------------
 
-# What if we need to work with the Access data in R again?
+# # #
+
+# What if we need to work with the Access data in R again? ---------------------
+
 # We can connect to the database and query from it
 # The connection is made in the same way
 database <- "data/prairie-fen-database.accdb"
 connection <- odbcConnectAccess2007(database)
-# We don't need to redo this while we're still in R,
-# but we do need to re-establish the connection each time we open R
+  # Rebuild the connection each time you open R
 
 # We can easily fetch each table
 location2 <- sqlFetch(connection, "location")
@@ -170,7 +172,7 @@ sqlQuery(connection,
 # For example, what species were observed in quadrats "BVF11" and "BVF12"?
 sqlQuery(connection,
          query = "SELECT DISTINCT(scientificName) FROM occurrence WHERE eventID IN ('BVF11', 'BVF12')")
-# In this case, we added DISTINCT() because we only want to know the species that  occurred
+# In this case, we added DISTINCT() because we only want to know the species that occurred
 # and we don't want any duplicates
 
 # We can also aggregate data in our queries
@@ -189,7 +191,7 @@ sqlQuery(connection,
                   WHERE occurrence.scientificName = 'Dasiphora fruticosa (L.) Rydb.' 
                     AND locationID = 'LAL'")
 
-################################################################################
+# # #
 
 # Sometimes when we start exploring our data further, we realize we need to make some changes
 # We can update the Access database from R
@@ -225,7 +227,7 @@ sqlQuery(connection,
 # in LAL9 should actually be Betula papyrifera, which is paper birch
 
 # We can update the record in the occurrence table,
-# but because we don't have a primary key Access actually won't let us update
+# but because we don't have a primary key Access actually won't let us update either
 # So we're going to basically replace the occurrence table
 sqlDrop(connection, "occurrence")
 sqlSave(connection,
@@ -239,7 +241,7 @@ sqlSave(connection,
 sqlQuery(connection,
          query = "SELECT scientificName FROM occurrence WHERE catalogNumber = 'LAL9-4'")
 
-################################################################################
+# # #
 
 # Often we want to save a query to an external file
 # Let's build a query, assign it to an object, then write it out as a flat file
@@ -253,12 +255,13 @@ prairie_sedge_points <- sqlQuery(connection,
                                           WHERE occurrence.scientificName = 'Carex prairea Dewey'")
 
 # We'll write it out as a text file for mapping somewhere else
-write.table(prairie_sedge_points, "data/prairie_sedge_points.txt",
+write.table(prairie_sedge_points, "output/prairie_sedge_points.txt",
             row.names = FALSE, col.names = TRUE, sep = "\t", quote = FALSE,
             fileEncoding = "UTF-8", append = FALSE)
 
-################################################################################
+# # #
+
+# Close the connection
+odbcClose(connection)
 
 # We started with a flat file and ended with a flat file, and did a lot in between!
-
-
